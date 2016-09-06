b5289dc904af3bb516099132382fbe340f33d4b9
Tsani Nashrullah <tsani.nashrullah@gmail.com>
2016-09-06 14:45:05 +0100

    add words 	in index.php and home.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<b>this is index bro</b>
+<b>this is index bro</git@github.com:arifakhriwilaga/exercise_collab.gitgit@github.com:arifakhriwilaga/exercise_collab.gitgit@github.com:arifakhriwilaga/exercise_collab.gitgit@github.com:arifakhriwilaga/exercise_collab.gitgit@github.com:arifakhriwilaga/exercise_collab.gitgit@github.com:arifakhriwilaga/exercise_collab.gitgit@github.com:arifakhriwilaga/exercise_collab.gitgit@github.com:arifakhriwilaga/exercise_collab.gitgit@github.com:arifakhriwilaga/exercise_collab.gitgit@github.com:arifakhriwilaga/exercise_collab.gitgit@github.com:arifakhriwilaga/exercise_collab.gitgit@github.com:arifakhriwilaga/exercise_collab.gitgit@github.com:arifakhriwilaga/exercise_collab.gitgit@github.com:arifakhriwilaga/exercise_collab.gitgit@github.com:arifakhriwilaga/exercise_collab.gitgit@github.com:arifakhriwilaga/exercise_collab.gitgit@github.com:arifakhriwilaga/exercise_collab.gitgit@github.com:arifakhriwilaga/exercise_collab.gitb>
 1.Ari ,
 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
